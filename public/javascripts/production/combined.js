2c361fa69cba42b781db81f7cf7587c934ec9fb2
Lachlan Hardy <lachlan@lachstock.com.au>
2010-04-05 15:53:25 +1000

    Minifying changed JS & CSS for production

--- public/javascripts/production/combined.js
+++ public/javascripts/production/combined.js
@@ -6003,4 +6003,12 @@ $(document).ready(function(){
   // making sexy unobtrusive CSS possible since 2006
   $("html").addClass("js");
 
+  Shadowbox.init({
+      continuous: true
+  });
+  
+  Shadowbox.setup("#gallery a", {
+      gallery: "Fairlane"
+  });
+
 });
